8caf77384700e15da38c603d7c381c87ea764fde
Joakim Edlund <joakim.en.edlund@gmail.com>
2015-10-13 15:09:05 +0200

    fixed parsing articles

--- news_generator.py
+++ news_generator.py
@@ -2,26 +2,41 @@ __author__ = 'joakim'
 
 import nltk
 from bs4 import BeautifulSoup
-import urllib.request
-
-baseUrl = "http://www.nytimes.com/pages/"
+import requests
 
 def main():
 
+    baseUrl = "http://www.nytimes.com/pages/"
+
     subject = "politics"
     url = baseUrl + subject + "/index.html"
-    page = urllib.request.urlopen(url)
+    page = requests.get(url, timeout=5)
 
-    soup = BeautifulSoup(page.read(), "html.parser")
+    soup = BeautifulSoup(page.content, "html.parser")
 
     div = soup.find("div", {"class", "searchResults"})
     links = soup.find_all('a', href=True)
-    articles = set([])
+    articleLinks = set([])
 
     for link in links:
-        if "www.nytimes.com" and "2015/10" in link['href'] and not "interactive" in link['href']: articles.add(link['href'])
+        if "www.nytimes.com" in link['href'] and "com/2015/10" in link['href'] and not "interactive" in link['href']:
+            articleLinks.add(link['href'])
 
-    print (articles)
+    articles = set()
+
+    for article in articleLinks:
+        sida = requests.get(article, timeout=5)
+        soppa = BeautifulSoup(sida.content, "html.parser")
+        header = soppa.find('', {"class": "story-heading"})
+        body = soppa.find('div', {"class": "story-body"})
+        bodytext = soppa.find_all('p', {"class": "story-content"})
+        articleText = ""
+        for t in bodytext:
+            articleText += t.text
+        articleTuple = (header, articleText)
+        articles.add(articleTuple)
+
+    #return articles
 
 if __name__ == "__main__":
     main()
